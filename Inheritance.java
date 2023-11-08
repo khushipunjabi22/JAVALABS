85903cc1dace7c7f0acf5af528699da885baf161
khushipunjabi22 <128999136+khushipunjabi22@users.noreply.github.com>
2023-11-08 12:19:41 +0530

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,43 @@
+class Book
+{
+void display() {
+System.out.println("Name-The complete Ref Java 2");
+System.out.println("Author-Herbert Schildt");
+System.out.println();
+}
+}
+class Reference_Book extends Book
+{
+    @Override
+void display(){
+System.out.println("Name-Pure Java 2");
+System.out.println("Author-Kenneth L");
+System.out.println();
+}
+}
+class Magazine extends Book
+{
+     @Override
+void display(){
+System.out.println("Name-Core Java");
+System.out.println("Author-Cay S");
+System.out.println();
+}
+}
+
+public class InheritanceDemo
+{
+public static void main(String[] args)
+{
+Book b;
+b=new Book();
+b.display();
+b=new Reference_Book();
+b.display();
+b=new Magazine();
+b.display();
+}
+}
+
+
+
